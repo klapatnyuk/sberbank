4249708d8ea1dbe771fdbec0e97d89407f60f0ba
vykla <vykla@ruspbnbvykla02.nts-gmbh.com>
2016-06-16 11:37:02 +0300

    Removed custom WidgetSet compilation

--- src/main/java/ru/klapatnyuk/SberbankUI.java
+++ src/main/java/ru/klapatnyuk/SberbankUI.java
@@ -2,7 +2,6 @@ package ru.klapatnyuk;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.*;
@@ -10,15 +9,10 @@ import com.vaadin.ui.*;
 import javax.servlet.annotation.WebServlet;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window
- * (or tab) or some part of a html page where a Vaadin application is embedded.
- * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be
- * overridden to add component to the user interface and initialize non-component functionality.
+ * @author klapatnyuk
  */
 @Theme("mytheme")
-@Widgetset("ru.klapatnyuk.MyAppWidgetset")
-public class MyUI extends UI {
+public class SberbankUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
@@ -40,8 +34,8 @@ public class MyUI extends UI {
         setContent(layout);
     }
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    @WebServlet(urlPatterns = "/*", name = "SberbankUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = SberbankUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
 }
