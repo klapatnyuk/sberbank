27f78f588499dd51aee0ab86d1dfc1a61115c9d5
vykla <vykla@ruspbnbvykla02.nts-gmbh.com>
2016-06-21 14:20:02 +0300

    Removed StylePostfix class

--- sberbank-web/src/main/java/ru/klapatnyuk/sberbank/web/StyleDimensions.java
+++ sberbank-web/src/main/java/ru/klapatnyuk/sberbank/web/StyleDimensions.java
@@ -1,7 +1,5 @@
 package ru.klapatnyuk.sberbank.web;
 
-import static ru.klapatnyuk.sberbank.web.StylePostfix.*;
-
 /**
  * @author klapatnyuk
  */
@@ -12,10 +10,10 @@ public interface StyleDimensions {
     Integer HEIGHT_L_RAW = 40;
     Integer HEIGHT_XL_RAW = 50;
 
-    String HEIGHT_S = "" + HEIGHT_S_RAW + PX;
-    String HEIGHT_M = "" + HEIGHT_M_RAW + PX;
-    String HEIGHT_L = "" + HEIGHT_L_RAW + PX;
-    String HEIGHT_XL = "" + HEIGHT_XL_RAW + PX;
+    String HEIGHT_S = HEIGHT_S_RAW + "px";
+    String HEIGHT_M = HEIGHT_M_RAW + "px";
+    String HEIGHT_L = HEIGHT_L_RAW + "px";
+    String HEIGHT_XL = HEIGHT_XL_RAW + "px";
 
     Integer WIDTH_XXS_RAW = 16;
     Integer WIDTH_XXS_BUTTON_RAW = 40;
@@ -27,34 +25,34 @@ public interface StyleDimensions {
     Integer WIDTH_XXL_RAW = 350;
     Integer WIDTH_XXXL_RAW = 700;
 
-    String WIDTH_XXS = "" + WIDTH_XXS_RAW + PX;
-    String WIDTH_XXS_BUTTON = "" + WIDTH_XXS_BUTTON_RAW + PX;
-    String WIDTH_XS = "" + WIDTH_XS_RAW + PX;
-    String WIDTH_S = "" + WIDTH_S_RAW + PX;
-    String WIDTH_M = "" + WIDTH_M_RAW + PX;
-    String WIDTH_L = "" + WIDTH_L_RAW + PX;
-    String WIDTH_XL = "" + WIDTH_XL_RAW + PX;
-    String WIDTH_XXL = "" + WIDTH_XXL_RAW + PX;
-    String WIDTH_XXXL = "" + WIDTH_XXXL_RAW + PX;
+    String WIDTH_XXS = WIDTH_XXS_RAW + "px";
+    String WIDTH_XXS_BUTTON = WIDTH_XXS_BUTTON_RAW + "px";
+    String WIDTH_XS = WIDTH_XS_RAW + "px";
+    String WIDTH_S = WIDTH_S_RAW + "px";
+    String WIDTH_M = WIDTH_M_RAW + "px";
+    String WIDTH_L = WIDTH_L_RAW + "px";
+    String WIDTH_XL = WIDTH_XL_RAW + "px";
+    String WIDTH_XXL = WIDTH_XXL_RAW + "px";
+    String WIDTH_XXXL = WIDTH_XXXL_RAW + "px";
 
     String WIDTH = WIDTH_M;
 
     Integer VIDEO_WIDTH_RAW = 704;
     Integer VIDEO_HEIGHT_RAW = 576;
 
-    String VIDEO_WIDTH = "" + VIDEO_WIDTH_RAW + PX;
-    String VIDEO_HEIGHT = "" + VIDEO_HEIGHT_RAW + PX;
+    String VIDEO_WIDTH = VIDEO_WIDTH_RAW + "px";
+    String VIDEO_HEIGHT = VIDEO_HEIGHT_RAW + "px";
 
     Integer SEPERATOR_HEIGHT_RAW = 1;
 
-    String SEPARATOR_HEIGHT = "" + SEPERATOR_HEIGHT_RAW + PX;
+    String SEPARATOR_HEIGHT = SEPERATOR_HEIGHT_RAW + "px";
 
     String WINDOW_WIDTH = "412px";
     String WINDOW_WIDTH_L = "425px";
     String WINDOW_WIDTH_XL = "500px";
     String WINDOW_WIDTH_XXL = "600px";
 
-    String WINDOW_VIDEO_WIDTH = "" + (VIDEO_WIDTH_RAW + 25 * 4) + PX;
+    String WINDOW_VIDEO_WIDTH = (VIDEO_WIDTH_RAW + 25 * 4) + "px";
 
     String WINDOW_HEIGHT = "400px";
     String WINDOW_HEIGHT_L = "500px";
