44144e97d7c62fec495422733a3bb07b2b30abe2
klapatnyuk <klapatnyuk@yandex.ru>
2016-06-22 01:10:06 +0300

    Initial data translated to russian

--- sberbank-model/src/main/resources/db/migration/V0_3__insert_initial_data.sql
+++ sberbank-model/src/main/resources/db/migration/V0_3__insert_initial_data.sql
@@ -5,48 +5,48 @@ INSERT INTO "user" (id, login, password, role) VALUES
     (2, 'client', 'd2a04d71301a8915217dd5faf81d12cffd6cd958', 'CLIENT');
 
 INSERT INTO template (id, title) VALUES
-    (1, 'Contract'),
-    (2, 'Request');
+    (1, 'Контракт'),
+    (2, 'Заявка');
 
 INSERT INTO template_field (id, template_id, title, label, type, "order") VALUES
-    (1, 1, 'surname', 'Surname', 'LINE', 0),
-    (2, 1, 'name', 'Name', 'LINE', 1),
-    (3, 1, 'description', 'Description', 'AREA', 2),
-    (4, 1, 'active', 'Active', 'CHECKBOX', 3),
+    (1, 1, 'surname', 'Фамилия', 'LINE', 0),
+    (2, 1, 'name', 'Имя', 'LINE', 1),
+    (3, 1, 'description', 'Описание', 'AREA', 2),
+    (4, 1, 'active', 'Активен', 'CHECKBOX', 3),
 
-    (5, 2, 'name', 'Name', 'LINE', 0),
-    (6, 2, 'phone', 'Phone', 'LINE', 1),
-    (7, 2, 'comment', 'Comments', 'AREA', 2);
+    (5, 2, 'name', 'Имя', 'LINE', 0),
+    (6, 2, 'phone', 'Телефон', 'LINE', 1),
+    (7, 2, 'comment', 'Комментарий', 'AREA', 2);
 
 INSERT INTO document (id, template_id, title, owner_id) VALUES
-    (1, 1, 'Contract 1', 1),
-    (2, 2, 'Request 1', 1),
+    (1, 1, 'Контракт №1', 1),
+    (2, 2, 'Запрос №1', 1),
 
-    (3, 1, 'Contract 2', 2),
-    (4, 1, 'Contract 3', 2),
-    (5, 2, 'Request 2', 2);
+    (3, 1, 'Контракт №2', 2),
+    (4, 1, 'Контракт №3', 2),
+    (5, 2, 'Запрос №2', 2);
 
 INSERT INTO document_field (id, document_id, template_field_id, value) VALUES
-    (1, 1, 1, 'Klapatnyuk'),
-    (2, 1, 2, 'Vyacheslav'),
-    (3, 1, 3, 'Abiding citizen'),
+    (1, 1, 1, 'Клапатнюк'),
+    (2, 1, 2, 'Вячеслав'),
+    (3, 1, 3, 'Законопослушный гражданин'),
     (4, 1, 4, 'Y'),
 
-    (5, 2, 5, 'Vyacheslav'),
+    (5, 2, 5, 'Вячеслав'),
     (6, 2, 6, '+7 (911) 032-36-30'),
 
-    (7, 3, 1, 'Ivanov'),
-    (8, 3, 2, 'Ivan'),
+    (7, 3, 1, 'Иванов'),
+    (8, 3, 2, 'Иван'),
     (9, 3, 4, 'N'),
 
-    (10, 4, 1, 'Ivanov'),
-    (11, 4, 2, 'Ivan'),
-    (12, 4, 3, 'The taxpayer'),
+    (10, 4, 1, 'Иванов'),
+    (11, 4, 2, 'Иван'),
+    (12, 4, 3, 'Налогоплательщик'),
     (13, 4, 4, 'Y'),
 
-    (14, 5, 5, 'Ivan'),
+    (14, 5, 5, 'Иван'),
     (15, 5, 6, '+7 (000) 000-00-00'),
-    (16, 5, 7, 'Call after 18:00');
+    (16, 5, 7, 'Звонить после 18:00');
 
 -- Rollback all actions
 
